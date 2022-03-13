72eddd605c60c21fd418859e0a0370711d55dd8b
LAPTOP <tiencuongkhoa71@gmail.com>
2022-03-13 19:42:41 +0700

    Tien commit GUI

--- app/src/main/java/com/example/doanappdkhp/MainActivity.java
+++ app/src/main/java/com/example/doanappdkhp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.doanappdkhp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnDangNhap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDangNhap = findViewById(R.id.btn_DangNhap);
+
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TrangChinh.class));
+            }
+        });
     }
 }
