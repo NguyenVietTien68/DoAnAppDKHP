4b58d3115528b58f7a77b177120d08226fcab806
LAPTOP <tiencuongkhoa71@gmail.com>
2022-03-22 12:48:10 +0700

    Tien commit

--- app/src/main/java/com/example/doanappdkhp/fragment/InfoStudent.java
+++ app/src/main/java/com/example/doanappdkhp/fragment/InfoStudent.java
@@ -1,13 +1,21 @@
 package com.example.doanappdkhp.fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.doanappdkhp.R;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +28,8 @@ public class InfoStudent extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private TextView txtName, txtID, txtGioiTinh, txtNgaySinh, txtDiaChi, txtKhoaHoc, txtSoDT;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -54,12 +64,46 @@ public class InfoStudent extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_info_student, container, false);
+        txtID = (TextView) view.findViewById(R.id.txtIdInfo);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_student, container, false);
+        new Async().execute();
+        return view;
+    }
+
+    class Async extends AsyncTask<Void, Void, Void> {
+        String records = "",error="";
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection con = DriverManager
+                        .getConnection("jdbc:mysql://192.168.0.4:3306/sqlquanlyhocphan","andro", "andro");
+                Statement statement = con.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM sinhvien WHERE MSSV = 0001 ");
+                while(resultSet.next()) {
+                    records += resultSet.getString(1);
+                }
+            }
+            catch(Exception e)
+            {
+                error = e.toString();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            txtID.setText(records);
+//            if(error != "")
+//                errorText.setText(error);
+            super.onPostExecute(aVoid);
+        }
     }
 }
